0cfa9157e33324351b0dee53454fa14b806438ef
ucharles28 <uzomacharles28@gmail.com>
2024-12-14 04:24:35 +0100

    add technologies component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import About from "@/components/About";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
+import Technologies from "@/components/Technologies";
 import Image from "next/image";
 
 export default function Home() {
@@ -9,6 +10,7 @@ export default function Home() {
       <Navbar />
       <Hero />
       <About />
+      <Technologies />
     </>
   );
 }

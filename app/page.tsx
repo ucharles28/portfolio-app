7a7e0f9ec50ef91385779881d6297b7dd8fcc3e2
ucharles28 <uzomacharles28@gmail.com>
2024-12-14 05:10:12 +0100

    Add contact section

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import About from "@/components/About";
+import Contact from "@/components/Contact";
 import Experience from "@/components/Experience";
 import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
@@ -15,6 +16,7 @@ export default function Home() {
       <Technologies />
       <Experience />
       <Projects />
+      <Contact />
     </>
   );
 }

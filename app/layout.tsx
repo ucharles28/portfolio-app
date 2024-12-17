fcb605b67eeb263a07f32e5f308faf8cac2edae8
ucharles28 <uzomacharles28@gmail.com>
2024-12-17 10:04:11 +0100

    Update contact page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Poppins } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
+const poppins = Poppins({ subsets: ["latin"], variable: '--font-poppins', weight: ['400', '500', '600'] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -17,16 +18,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className}`}
+        className={`${poppins.className}`}
       >
         <div className="overflow-x-hidden overflow-y-auto text-neutral-300 slec selection:bg-cyan-300 selection:text-cyan-900 antialiased">
-          <div className=" top-0 -z-10 h-full w-full">
+          <div className="fixed top-0 -z-10 h-full w-full">
             <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
+          </div>
             <div className="container mx-auto px-8">
               {children}
             </div>
-
-          </div>
 
         </div>
       </body>
--- components/Contact.tsx
+++ components/Contact.tsx
@@ -1,14 +1,70 @@
 import { CONTACT } from "@/constants"
+import { FaSquareXTwitter } from 'react-icons/fa6'
+import { FaLinkedin } from "react-icons/fa";
+import { FaTelegram } from "react-icons/fa";
+import { IoSend } from "react-icons/io5";
 
 const Contact = () => {
   return (
     <div>
-      <div className="border-b border-neutral-900 pb-20">
-        <h1 className="my-10 text-center text-4xl">Get in Touch</h1>
-        <div className="text-center trackPing-tighter">
-            <p className="my-4">{CONTACT.address}</p>
-            <p className="my-4">{CONTACT.phoneNo}</p>
-            <a href="#" className="border-b">{CONTACT.email}</a>
+      <div className="border-b border-neutral-900 pb-20 font-inter">
+        <h1 className="my-10 text-center text-4xl">Contact Me</h1>
+        <div className="flex">
+          <div className="flex flex-col lg:flex-row w-full lg:justify-center gap-16 md:gap-20">
+            <div className="flex flex-col gap-10">
+              <div className="flex gap-3 items-center">
+                <FaSquareXTwitter className="text-purple-800 text-3xl" />
+                <a className="flex flex-col gap-1" href="https://x.com/callmeuzor" target="_blank">
+                  <p className="text-lg font-medium">Twitter Profile</p>
+                  <p className="text-neutral-400 text-xs">@callmeuzor</p>
+                </a>
+              </div>
+
+              <div className="flex gap-3 items-center">
+                <FaLinkedin className="text-purple-800 text-3xl" />
+                <a className="flex flex-col gap-1" href="https://www.linkedin.com/in/ike-uzoma-charles-946156178/" target="_blank">
+                  <p className="text-lg font-medium">LinkedIn Profile</p>
+                  <p className="text-neutral-400 text-xs">@callmeuzor_</p>
+                </a>
+              </div>
+
+              <div className="flex gap-3 items-center">
+                <FaTelegram className="text-purple-800 text-3xl" />
+                <a className="flex flex-col gap-1" href="https://t.me/uzomacharles" target="_blank">
+                  <p className="text-lg font-medium">Telegram Username</p>
+                  <p className="text-neutral-400 text-xs">@uzomacharles</p>
+                </a>
+              </div>
+            </div>
+            <div className="flex flex-col gap-5">
+              <div className="flex lg:flex-row flex-col w-full gap-5">
+                <div className="flex flex-col gap-2 rounded-lg bg-[#1f1f20] px-4 pb-2 pt-3">
+                  <p className="">Name</p>
+                  <input type="text" className="outline-none border-0 bg-transparent" />
+                </div>
+                <div className="flex flex-col gap-2 rounded-lg bg-[#1f1f20] px-4 pb-2 pt-3">
+                  <p className="">Email</p>
+                  <input type="text" className="outline-none border-0 bg-transparent" />
+                </div>
+              </div>
+              <div className="flex flex-col gap-2 rounded-lg bg-[#1f1f20] px-4 pb-2 pt-3">
+                <p className="">What type of app do you need?</p>
+                <input type="text" className="outline-none border-0 bg-transparent" />
+              </div>
+              <div className="flex flex-col gap-2 rounded-lg bg-[#1f1f20] px-4 pb-2 pt-3">
+                <p className="">Additional message</p>
+                <textarea className="outline-none border-0 bg-transparent" rows={3}></textarea>
+              </div>
+              <div className="flex justify-start">
+                <button className="flex bg-purple-800 px-4 py-2 gap-3 rounded items-center text-white">
+                  <p className="">Send Message</p>
+                  <IoSend className="text-xl" />
+                </button>
+
+              </div>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
